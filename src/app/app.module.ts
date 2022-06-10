52e458fca036e80b9218b7526b7db44579f6427c
Thomas Wesselink <thomas.wesselink@ing.com>
2022-06-10 13:59:20 +0200

    Add todolist html/css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TodolistComponent } from './home/todolist/todolist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
