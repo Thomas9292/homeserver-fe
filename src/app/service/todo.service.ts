cbe299101bfd0a99303abca14e143f3f43d23198
Thomas Wesselink <thomas.wesselink@ing.com>
2022-06-10 20:25:06 +0200

    Add config environments

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, share, Subject, Subscription } from 'rxjs';
-import { SubscriptionLog } from 'rxjs/internal/testing/SubscriptionLog';
+import { environment } from 'src/environments/environment';
 import { Todo } from '../model/Todo';
 
-const TODO_ENDPOINT = 'http://localhost:8080/api/v1/todo';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,29 +15,31 @@ export class TodoService {
   getTodosSubscription: Subscription;
   addTodoSubscription: Subscription;
 
+  todoEndpointRoot: string = environment.server_domain + '/api/v1/todo';
+
   constructor(private http: HttpClient) { }
 
   refreshTodos(): void {
-    this.getTodosSubscription = this.http.get<Todo[]>(TODO_ENDPOINT).subscribe((todos) => {
+    this.getTodosSubscription = this.http.get<Todo[]>(this.todoEndpointRoot).subscribe((todos) => {
       this.todos = todos;
       this.todosUpdated.next(todos);
     });
   }
 
   updateTodo(todo: Todo): Observable<Todo> {
-    return this.http.put<Todo>(TODO_ENDPOINT + "/" + todo.id, todo);
+    return this.http.put<Todo>(this.todoEndpointRoot + "/" + todo.id, todo);
   }
 
   deleteTodo(todo: Todo): void {
     this.todos = this.todos.filter(t => todo.id != t.id);
     this.todosUpdated.next(this.todos);
-    this.http.delete(TODO_ENDPOINT + "/" + todo.id, { observe: "response"}).subscribe(resp => {
+    this.http.delete(this.todoEndpointRoot + "/" + todo.id, { observe: "response"}).subscribe(resp => {
       this.refreshTodos();
     });
   }
 
   addTodo(todo: Todo): Observable<Todo> {
-    const eventRequest = this.http.post<Todo>(TODO_ENDPOINT, todo).pipe(share());
+    const eventRequest = this.http.post<Todo>(this.todoEndpointRoot, todo).pipe(share());
     this.getTodosSubscription = eventRequest.subscribe(todo => {
       this.todos.push(todo);
       this.todosUpdated.next(this.todos);
